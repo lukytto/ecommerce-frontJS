d2aa389c99a7d6e58c0de9e1b486bed72c20d5c5
lukas.kup <lukas.s.kuprys@gmail.com>
2020-04-10 16:02:33 +0300

    front-end

---
+++ src/core/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => (
+    <Layout title='Home Page' description='Node React ecommerce'>
+
+    </Layout>);
+
+export default Home;

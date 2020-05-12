4e7f4c7f3403f0e1c7d7340b78e3c9034498deb1
lukas.kup <lukas.s.kuprys@gmail.com>
2020-05-12 17:36:17 +0300

    update card display

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
-import {getCategories, getSub_categories, getSub_sub_categories, getFilteredProducts } from './apiCore';
+import { getCategories, getSub_categories, getSub_sub_categories, getFilteredProducts } from './apiCore';
 import CheckBox from './Checkbox';
-import {createPriceSlider, createThicknessSlider} from './Sliders';
+import { createPriceSlider, createThicknessSlider } from './Sliders';
 
 import noUiSlider from 'nouislider';
 import "nouislider/distribute/nouislider.css";
@@ -11,52 +11,53 @@ import wNumb from 'wnumb';
 
 const Shop = () => {
     const [myFilters, setMyFilters] = useState({
-        filters: { category: [], sub_category: [], sub_sub_category: [], price: [], 'parameters.thickness': []}
+        filters: { category: [], sub_category: [], sub_sub_category: [], price: [], 'parameters.thickness': [] }
     });
     const [categories, setCategories] = useState([]);
     const [sub_categories, setSub_categories] = useState([]);
     const [sub_sub_categories, setSub_sub_categories] = useState([]);
     const [error, setError] = useState(false);
-    const [limit, setLimit] = useState(6);
+    const [limit, setLimit] = useState(12);
     const [skip, setSkip] = useState(0);
     const [size, setSize] = useState(0);
     const [filteredResults, setFilteredResults] = useState([]);
-	let priceSlider, thicknessSlider;
+    let priceSlider, thicknessSlider;
 
-    const init = () => {;
-		
-		priceSlider = createPriceSlider(document.getElementById('priceSlider'));
-		thicknessSlider = createThicknessSlider(document.getElementById('thicknessSlider'));
-		priceSlider.noUiSlider.on('change', handlePriceSlider);
-		thicknessSlider.noUiSlider.on('change', handleThicknessSlider);
-		
-		getCategories().then(data => {
+    const init = () => {
+        ;
+
+        priceSlider = createPriceSlider(document.getElementById('priceSlider'));
+        thicknessSlider = createThicknessSlider(document.getElementById('thicknessSlider'));
+        priceSlider.noUiSlider.on('change', handlePriceSlider);
+        thicknessSlider.noUiSlider.on('change', handleThicknessSlider);
+
+        getCategories().then(data => {
             if (data.error) {
                 setError(data.error);
             } else {
                 setCategories(data);
             }
         });
-		getSub_categories().then(data => {
+        getSub_categories().then(data => {
             if (data.error) {
                 setError(data.error);
             } else {
                 setSub_categories(data);
             }
         });
-		getSub_sub_categories().then(data => {
+        getSub_sub_categories().then(data => {
             if (data.error) {
                 setError(data.error);
             } else {
                 setSub_sub_categories(data);
             }
         });
-		
+
     };
 
     const loadFilteredResults = newFilters => {
         // console.log(newFilters);
-		//alert(newFilters);
+        //alert(newFilters);
         getFilteredProducts(skip, limit, newFilters).then(data => {
             if (data.error) {
                 setError(data.error);
@@ -99,48 +100,48 @@ const Shop = () => {
     }, []);
 
     const handleFilters = (filters, filterBy) => {
-		
+
         const newFilters = { ...myFilters }; //nukopijuoju myFilters i newFilters
-		
-		newFilters.filters[filterBy] = filters;
+
+        newFilters.filters[filterBy] = filters;
 
         loadFilteredResults(myFilters.filters);
         setMyFilters(newFilters);
     };
-	
-	const handlePriceSlider = (values, handle) => {
-		if(values != null) {
-			values[0] = Number(values[0].replace(/[^0-9.-]+/g,""));
-			values[1] = Number(values[1].replace(/[^0-9.-]+/g,""));
-			
-			let min = priceSlider.noUiSlider.options.range.min;
-			let max = priceSlider.noUiSlider.options.range.max;
-			
-			if(values[0] == min && values[1] == max) {
-				handleFilters([],'price');
-			} 	else {
-				handleFilters(values,'price');
-			}
-		}	
-	}
-	
-	const handleThicknessSlider = (values, handle) => {
-		if(values != null) {
-		
-			values[0] = Number(values[0].replace(/[^0-9.-]+/g,""));
-			values[1] = Number(values[1].replace(/[^0-9.-]+/g,""));
-			
-			let min = thicknessSlider.noUiSlider.options.range.min;
-			let max = thicknessSlider.noUiSlider.options.range.max;
-			
-			if(values[0] == min && values[1] == max) {
-				handleFilters([],'parameters.thickness');
-			} else {
-				handleFilters(values,'parameters.thickness');
-			}
-		}
-	}
-	
+
+    const handlePriceSlider = (values, handle) => {
+        if (values != null) {
+            values[0] = Number(values[0].replace(/[^0-9.-]+/g, ""));
+            values[1] = Number(values[1].replace(/[^0-9.-]+/g, ""));
+
+            let min = priceSlider.noUiSlider.options.range.min;
+            let max = priceSlider.noUiSlider.options.range.max;
+
+            if (values[0] == min && values[1] == max) {
+                handleFilters([], 'price');
+            } else {
+                handleFilters(values, 'price');
+            }
+        }
+    };
+
+    const handleThicknessSlider = (values, handle) => {
+        if (values != null) {
+
+            values[0] = Number(values[0].replace(/[^0-9.-]+/g, ""));
+            values[1] = Number(values[1].replace(/[^0-9.-]+/g, ""));
+
+            let min = thicknessSlider.noUiSlider.options.range.min;
+            let max = thicknessSlider.noUiSlider.options.range.max;
+
+            if (values[0] == min && values[1] == max) {
+                handleFilters([], 'parameters.thickness');
+            } else {
+                handleFilters(values, 'parameters.thickness');
+            }
+        }
+    };
+
 	/*
 	Kaip padaryti, kad viena checkboxa paspaudus atsidarytu kitas:
 	Pirma, reikia nustatyti, kad butent tas checkboxas pakrautas
@@ -155,67 +156,67 @@ const Shop = () => {
             className='container-fluid'>
 
             <div className='row'>
-                <div className='col-2'>			
-				
-					<div >
-						<h4>Filter by price</h4>
-						<br />
-						<div id= "priceSlider">
-						
-						</div>
-						<br /><br />
-					</div>
-					
-					<div >
-						<h4>Filter by thickness</h4>
-						<br />
-						<div id= "thicknessSlider">
-						
-						</div>
-						<br /><br />
-					</div>
-					
-					<div>
-						<h4>Filter by category</h4>
-						<ul>
-							<CheckBox categories={categories}
-								handleFilters={filters =>
-									handleFilters(filters, 'category')} />
-						</ul>
-					</div>
+                <div className='col-2'>
 
-					<div>
-						<h4>Filter by subcategory</h4>
-						<ul>
-							<CheckBox categories={sub_categories}
-								handleFilters={filters =>
-									handleFilters(filters, 'sub_category')} />
-						</ul>					
-					</div>
-					
-					<div>
-						<h4>Filter by type</h4>
-						<ul>
-							<CheckBox categories={sub_sub_categories}
-								handleFilters={filters =>
-									handleFilters(filters, 'sub_sub_category')} />
-						</ul>					
-					</div>
-					
+                    <div >
+                        <h4>Filter by price</h4>
+                        <br />
+                        <div id="priceSlider">
+
+                        </div>
+                        <br /><br />
+                    </div>
+
+                    <div >
+                        <h4>Filter by thickness</h4>
+                        <br />
+                        <div id="thicknessSlider">
+
+                        </div>
+                        <br /><br />
+                    </div>
+
+                    <div>
+                        <h4>Filter by category</h4>
+                        <ul>
+                            <CheckBox categories={categories}
+                                handleFilters={filters =>
+                                    handleFilters(filters, 'category')} />
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4>Filter by subcategory</h4>
+                        <ul>
+                            <CheckBox categories={sub_categories}
+                                handleFilters={filters =>
+                                    handleFilters(filters, 'sub_category')} />
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4>Filter by type</h4>
+                        <ul>
+                            <CheckBox categories={sub_sub_categories}
+                                handleFilters={filters =>
+                                    handleFilters(filters, 'sub_sub_category')} />
+                        </ul>
+                    </div>
+
                 </div>
 
                 <div className='col-10'>
                     <h2 className='mb4'>Products</h2>
                     <div className='row'>
-					
+
                         {filteredResults.map((product, i) => (
 
-                            <div key={i} className='col-4 mb-4'>
+                            <div key={i} className='col-2 mb-2'>
                                 <Card product={product} />
                             </div>
 
                         ))}
-						
+
                     </div>
                     <hr />
                     {loadMoreButton()}

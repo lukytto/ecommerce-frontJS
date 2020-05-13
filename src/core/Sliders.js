092880d680623ee564a6725ce51cffe604e45cc5
lukas.kup <lukas.s.kuprys@gmail.com>
2020-05-13 14:00:15 +0300

    style update

--- src/core/Sliders.js
+++ src/core/Sliders.js
@@ -5,61 +5,60 @@ import wNumb from 'wnumb';
 export const createPriceSlider = (slider) => {
 
 	noUiSlider.create(slider, {
-	connect: true,
-	animate: false,
-	tooltips: true,
-	//step: 1,
-	pips: {
-		mode: 'range',
-		density: 5,
+		connect: true,
+		animate: false,
+		tooltips: true,
+		//step: 1,
+		pips: {
+			mode: 'range',
+			density: 5,
 			format: wNumb({
 				decimals: 0,
 				prefix: '€'
 			})
-	},
-	start: [0, 1000],
-	range: {
-		'min': 0,
-		'50%': 100,
-		'80%': 200,
-		'max': 1000
-	},
-	format: wNumb({
-		decimals: 2,
-		prefix: '€'
-	})
+		},
+		start: [0, 1000],
+		range: {
+			'min': 0,
+			'50%': 100,
+			'80%': 200,
+			'max': 1000
+		},
+		format: wNumb({
+			decimals: 2,
+			prefix: '€'
+		})
 	});
-	
+
 	return slider;
-}
+};
 
 export const createThicknessSlider = (slider) => {
-	
+
 	noUiSlider.create(slider, {
-	connect: true,
-	animate: false,
-	tooltips: true,
-	//step: 1,
-	pips: {
-		mode: 'range',
-		density: 5,
+		connect: true,
+		animate: false,
+		tooltips: true,
+		//step: 1,
+		pips: {
+			mode: 'range',
+			density: 5,
 			format: wNumb({
 				decimals: 0,
 				postfix: 'mm'
 			})
-	},
-	start: [0, 100],
-	range: {
-		'min': 0,
-		'max': 100
-	},
-	format: wNumb({
-		decimals: 2,
-		postfix: 'mm'
-	})
+		},
+		start: [0, 100],
+		range: {
+			'min': 0,
+			'max': 100
+		},
+		format: wNumb({
+			decimals: 2,
+			postfix: 'mm'
+		})
 	});
-	
-	return slider;
-}
 
-	
+	return slider;
+};
+

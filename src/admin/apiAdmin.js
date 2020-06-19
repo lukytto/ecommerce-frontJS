5611b9dd159914061e9b8543452a3514c5c8751b
lukytto <lukas.s.kuprys@gmail.com>
2020-06-19 19:33:18 +0300

    orders front-end

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,100 +1,168 @@
-import { API } from '../config';
+import { API } from "../config";
 
 export const createCategory = (userId, token, category) => {
-    // console.log(name, email, password);
-    return fetch(`${API}/category/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(category)
+  // console.log(name, email, password);
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const updateCategory = (categoryId, userId, token, category) => {
+  return fetch(`${API}/category/${categoryId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      // content type?
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
 
 export const createProduct = (userId, token, product) => {
-    // console.log(name, email, password);
-    return fetch(`${API}/product/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${token}`
-        },
-        body: product
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 export const createSupplier = (userId, token, supplier) => {
-    return fetch(`${API}/supplier/create/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${token}`
-        },
-        body: supplier
+  return fetch(`${API}/supplier/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: supplier,
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
+export const getCategory = (categoryId) => {
+  return fetch(`${API}/category/${categoryId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
 export const getCategories = () => {
-	//const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
-	    return fetch(`${API}/categories`, {
-        method: "GET"
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
-
 
 export const getSub_categories = () => {
-	//const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
-	    return fetch(`${API}/sub_categories`, {
-        method: "GET"
+  //const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
+  return fetch(`${API}/sub_categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 export const getSub_sub_categories = () => {
-	//const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
-	    return fetch(`${API}/sub_sub_categories`, {
-        method: "GET"
+  //const categories = ["Windows and Wedges","Lenses","Mirrors","Optical Filters","Beamsplitters","Polarizers","Waveplates"];
+  return fetch(`${API}/sub_sub_categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
-
 export const getSuppliers = () => {
-    return fetch(`${API}/suppliers`, {
-        method: "GET"
+  return fetch(`${API}/suppliers`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
+};
+
+export const listOrders = (userId, token) => {
+  return fetch(`${API}/order/list/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const getStatusValues = (userId, token) => {
+  return fetch(`${API}/order/status-values/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+  return fetch(`${API}/order/${orderId}/status/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ status, orderId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Layout from "./Layout";
 import {
   getProducts,
   getBraintreeClientToken,
   processPayment,
+  createOrder,
 } from "./apiCore";
 import { emptyCart } from "./cartHelpers";
 import Card from "./Card";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+// import "braintree-web"; // not using this package
 import DropIn from "braintree-web-drop-in-react";
 
 const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
@@ -18,7 +19,7 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     clientToken: null,
     error: "",
     instance: {},
-    adress: "",
+    address: "",
   });
 
   const userId = isAuthenticated() && isAuthenticated().user._id;
@@ -27,8 +28,10 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
   const getToken = (userId, token) => {
     getBraintreeClientToken(userId, token).then((data) => {
       if (data.error) {
+        console.log(data.error);
         setData({ ...data, error: data.error });
       } else {
+        console.log(data);
         setData({ clientToken: data.clientToken });
       }
     });
@@ -38,6 +41,10 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     getToken(userId, token);
   }, []);
 
+  const handleAddress = (event) => {
+    setData({ ...data, address: event.target.value });
+  };
+
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
@@ -46,30 +53,32 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
 
   const showCheckout = () => {
     return isAuthenticated() ? (
-      <div>{showDropIn()} </div>
+      <div>{showDropIn()}</div>
     ) : (
       <Link to="/signin">
-        <button className="btn btn-primary">Sign in to checkout </button>{" "}
+        <button className="btn btn-primary">Sign in to checkout</button>
       </Link>
     );
   };
 
+  let deliveryAddress = data.address;
+
   const buy = () => {
     setData({ loading: true });
-    //send the nonce to your server
-    //nonce=data.instance.requestPaymentMethod()
+    // send the nonce to your server
+    // nonce = data.instance.requestPaymentMethod()
     let nonce;
-    let getNone = data.instance
+    let getNonce = data.instance
       .requestPaymentMethod()
       .then((data) => {
-        //console.log(data);
+        // console.log(data);
         nonce = data.nonce;
-        //once you have nonce(card type, card number) send nonce as 'paymentMethodNonce'
-        //and also total to be charged
+        // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
+        // and also total to be charged
         // console.log(
-        //   "send nonce and total to process: ",
-        //   nonce,
-        //   getTotal(products)
+        //     "send nonce and total to process: ",
+        //     nonce,
+        //     getTotal(products)
         // );
         const paymentData = {
           paymentMethodNonce: nonce,
@@ -78,18 +87,32 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
 
         processPayment(userId, token, paymentData)
           .then((response) => {
-            //console.log(response)
-            setData({ ...data, success: response.success });
-            emptyCart(() => {
-              setRun(!run); // update parent state
-              console.log("payment success and empty cart");
-              setData({
-                loading: false,
-                success: true,
+            console.log(response);
+            // empty cart
+            // create order
+
+            const createOrderData = {
+              products: products,
+              transaction_id: response.transaction.id,
+              amount: response.transaction.amount,
+              address: deliveryAddress,
+            };
+
+            createOrder(userId, token, createOrderData)
+              .then((response) => {
+                emptyCart(() => {
+                  setRun(!run); // run useEffect in parent Cart
+                  console.log("payment success and empty cart");
+                  setData({
+                    loading: false,
+                    success: true,
+                  });
+                });
+              })
+              .catch((error) => {
+                console.log(error);
+                setData({ loading: false });
               });
-            });
-            //empty cart
-            //create order
           })
           .catch((error) => {
             console.log(error);
@@ -97,7 +120,7 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
           });
       })
       .catch((error) => {
-        //console.log("dropin error: ", error);
+        // console.log("dropin error: ", error);
         setData({ ...data, error: error.message });
       });
   };
@@ -106,6 +129,16 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     <div onBlur={() => setData({ ...data, error: "" })}>
       {data.clientToken !== null && products.length > 0 ? (
         <div>
+          <div className="gorm-group mb-3">
+            <label className="text-muted">Delivery address:</label>
+            <textarea
+              onChange={handleAddress}
+              className="form-control"
+              value={data.address}
+              placeholder="Type your delivery address here..."
+            />
+          </div>
+
           <DropIn
             options={{
               authorization: data.clientToken,
@@ -115,7 +148,6 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
             }}
             onInstance={(instance) => (data.instance = instance)}
           />
-
           <button onClick={buy} className="btn btn-success btn-block">
             Pay
           </button>
@@ -142,11 +174,12 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     </div>
   );
 
-  const showLoading = (loading) => loading && <h2>Loading...</h2>;
+  const showLoading = (loading) =>
+    loading && <h2 className="text-danger">Loading...</h2>;
 
   return (
     <div>
-      <h2> Total: $ {getTotal()} </h2>
+      <h2>Total: ${getTotal()}</h2>
       {showLoading(data.loading)}
       {showSuccess(data.success)}
       {showError(data.error)}
